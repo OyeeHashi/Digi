a187e1ee21d6de11470ff7261e2358d59736622a
OyeeHashi <hashimtop10@gmail.com>
2022-05-26 23:44:32 +0500

    Scroll, Jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 
     $(".about").waypoint(function(direction){
@@ -5,17 +7,40 @@ $(document).ready(function(){
         $("nav").addClass('sticky-nav');
         else
         $("nav").removeClass('sticky-nav');
-
+        
+    
     });
 
 
 
     $(".about").waypoint(function(direction){
-        if(direction=="down")
-         $("nav").addClass('animate__animated');
-         else
-         $("nav").removeClass('animate__animated');
- 
-     });
+        if(direction=='down'){
+            $(".about-sec").addClass('fade');
+            $(".about-img").addClass('fade');}
+            else{
+            $(".about-sec").removeClass('fade');
+            $(".about-img").removeClass('fade');}
 
+    
+    },{offset:'50px'}
+    );
+
+
+
+    $(".nav a").click(function(e){
+        if(this.hash!==''){
+            e.preventDefault();
+            const hash= this.hash;
+            $('html,body').animate({
+                scrollTop: $(hash).offset().top
+
+
+            },1000
+            )
+        }
+
+
+    });
+    
 });
+
